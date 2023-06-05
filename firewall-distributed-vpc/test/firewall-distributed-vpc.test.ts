761f9d7d1ff770ec162086b9641cd754c01cbed1
Richard Kang <2356020+kangks@users.noreply.github.com>
2023-06-05 23:18:07 +0800

    added rulesString test case

--- firewall-distributed-vpc/test/firewall-distributed-vpc.test.ts
+++ firewall-distributed-vpc/test/firewall-distributed-vpc.test.ts
@@ -1,18 +1,36 @@
-// import * as cdk from 'aws-cdk-lib';
-// import { Template } from 'aws-cdk-lib/assertions';
-// import * as FirewallDistributedVpc from '../lib/index';
+import * as cdk from 'aws-cdk-lib';
+import { Match, Template } from 'aws-cdk-lib/assertions';
+import * as FirewallDistributedVpc from '../lib/FirewallDistributedVpcConstruct';
 
 // example test. To run these tests, uncomment this file along with the
 // example resource in lib/index.ts
-test('SQS Queue Created', () => {
-//   const app = new cdk.App();
-//   const stack = new cdk.Stack(app, "TestStack");
-//   // WHEN
-//   new FirewallDistributedVpc.FirewallDistributedVpc(stack, 'MyTestConstruct');
-//   // THEN
-//   const template = Template.fromStack(stack);
+test('Firewall Created', () => {
+  const app = new cdk.App();
+  const stack = new cdk.Stack(app, "TestStack");
+  const vpc = new cdk.aws_ec2.Vpc(stack, "vpc", {
+    cidr: "10.0.0.0/24"
+  });
+  // WHEN
+  new FirewallDistributedVpc.FirewallDistributedVpc(stack, 'MyTestConstruct', {
+    vpc: vpc,
+    subnetList: [
+        new cdk.aws_ec2.Subnet(stack, "subnet", {
+            vpcId: vpc.vpcId,
+            cidrBlock: "10.0.0.0/25",
+            availabilityZone: vpc.availabilityZones[0]
+        })
+    ],
+    rulesFile: ["./test/rules.txt"]
+  });
+  // THEN
+  const template = Template.fromStack(stack);
 
-//   template.hasResourceProperties('AWS::SQS::Queue', {
-//     VisibilityTimeout: 300
-//   });
+  template.hasResourceProperties('AWS::NetworkFirewall::RuleGroup', 
+  {
+    RuleGroup:{
+        RulesSource:{
+            "RulesString": Match.anyValue(),
+        }
+    }
+  });
 });
